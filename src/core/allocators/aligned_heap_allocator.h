3b17ccfd90336621751695add8e53d75a2d119c6
julian lalu <julian.lalu@gmail.com>
2023-02-08 13:40:44 +0100

    Split aligned_heap_allocator::allocate to catch exceptions

--- src/core/allocators/aligned_heap_allocator.h
+++ src/core/allocators/aligned_heap_allocator.h
@@ -24,7 +24,12 @@ namespace hud
         template<typename type_t = u8>
         [[nodiscard]] constexpr allocation_type<type_t> allocate(const usize count) noexcept
         {
-            return count > 0 ? allocation_type<type_t>(static_cast<type_t *>(memory::allocate_align<type_t>(count, alignment)), count) : allocation_type<type_t> {};
+            if( count > 0u){
+                type_t* alloc = memory::allocate_align<type_t>(count, alignment);
+                return allocation_type<type_t>(alloc, count);
+            }
+            return allocation_type<type_t> {};
+            //return count > 0 ? allocation_type<type_t>(static_cast<type_t *>(memory::allocate_align<type_t>(count, alignment)), count) : allocation_type<type_t> {};
         }
 
         /**
