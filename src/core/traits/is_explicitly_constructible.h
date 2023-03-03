af34f70c8a787049b322ac720b65f52b29ef0a62
Julian LALU <julian.lalu@outlook.fr>
2023-03-03 17:32:03 +0100

    Fix is_explicit_constructible

--- src/core/traits/is_explicitly_constructible.h
+++ src/core/traits/is_explicitly_constructible.h
@@ -1,54 +1,16 @@
 #ifndef HD_INC_CORE_TRAITS_IS_EXPLICITLY_CONSTRUCTIBLE_H
 #define HD_INC_CORE_TRAITS_IS_EXPLICITLY_CONSTRUCTIBLE_H
-#include "../traits/is_constructible.h"
-#include "../traits/is_convertible.h"
-#include "../traits/void_t.h"
-#include "../templates/declval.h"
+#include "conjunction.h"
+#include "is_constructible.h"
+#include "negation.h"
+#include "is_implicitly_constructible.h"
 
 namespace hud
 {
-
-    namespace details
-    {
-        template<typename type_t>
-        constexpr void implicit_construct(const type_t &) noexcept;
-
-        template<typename void_t, bool is_constructible, typename type_t, typename... args_t>
-        struct is_explicitly_constructible_impl_2
-            : public hud::bool_constant<is_constructible>
-        {
-        };
-
-        template<typename type_t, typename... args_t>
-        struct is_explicitly_constructible_impl_2<hud::void_t<decltype(details::implicit_construct<type_t>({hud::declval<args_t>()...}))>, true, type_t, args_t...>
-            : public hud::false_type
-        {
-        };
-
-        template<typename type_t, typename... args_t>
-        struct is_explicitly_constructible_impl
-            : is_explicitly_constructible_impl_2<void, hud::is_constructible_v<type_t, args_t...>, type_t, args_t...>
-        {
-        };
-
-        template<typename type_t>
-        struct is_explicitly_constructible_impl<type_t, const type_t &>
-            : hud::bool_constant<!hud::is_convertible_v<const type_t &, type_t>>
-        {
-        };
-
-        template<typename type_t>
-        struct is_explicitly_constructible_impl<type_t, type_t &&>
-            : hud::bool_constant<!hud::is_convertible_v<type_t &&, type_t>>
-        {
-        };
-
-    } // namespace details
-
     /** Checks whether type_t is an explicitly constructible type with args_t. */
     template<typename type_t, typename... args_t>
     struct is_explicitly_constructible
-        : details::is_explicitly_constructible_impl<type_t, args_t...>
+        : hud::conjunction<hud::is_constructible<type_t, args_t...>, hud::negation<hud::is_implicitly_constructible<type_t, args_t...>>>
     {
     };
 
