3a727cdfc155b5b3cd8b7f00480df9e3c734df74
Julian LALU <julian.lalu@outlook.com>
2023-07-06 17:24:04 +0200

    Fix gcc tests

--- test/atomics.cpp
+++ test/atomics.cpp
@@ -622,7 +622,7 @@ GTEST_TEST(atomic, default_constructor_of_pointer_do_not_initialize_atomic)
         hud::memory::set(a, hud::u8_max);
 
         hud::atomic<type_t*>* atomic = new (a) hud::atomic<type_t*>;
-        hud_assert_ne(atomic->load(), nullptr); });
+        hud_assert_ne(atomic->load(), (type_t*)nullptr); });
 }
 
 GTEST_TEST(atomic, constructor_of_integral_initialize_atomic_to_value)
@@ -631,7 +631,7 @@ GTEST_TEST(atomic, constructor_of_integral_initialize_atomic_to_value)
     hud_test::for_each_type<i8, u8, i16, u16, i32, u32, i64, u64, usize, isize>()([]<typename type_t>()
                                                                                   {
         hud::atomic<type_t> a(32);
-        hud_assert_eq(a.load(), type_t{ 32 }); });
+        hud_assert_eq( a.load(), type_t{ 32 }); });
 }
 
 GTEST_TEST(atomic, constructor_of_pointer_with_tag_initialize_atomic_to_value)
--- test/tuple/constructors.cpp
+++ test/tuple/constructors.cpp
@@ -212,10 +212,9 @@ GTEST_TEST(tuple, default_constructor_uninit)
         [[maybe_unused]] tuple_type tuple1;
 
         tuple_type2 *tuple2 = hud::memory::allocate_array<tuple_type2>(1);
-        hud::memory::set(tuple2, sizeof(tuple_type2), 0xFF);
-        // Avoid optimisation in release that can remove the memory::set
-        static_cast<volatile tuple_type2 *>(tuple2);
-        hud::memory::construct_at(tuple2);
+        // Be sure optimizer don't remove the memmory::set
+        volatile void *do_not_optimize_set_tuple2 = hud::memory::set(tuple2, sizeof(tuple_type2), 0xFF);
+        hud::memory::construct_at(static_cast<volatile tuple_type2 *>(do_not_optimize_set_tuple2));
         hud_assert_true((hud::is_same_v<decltype(hud::get<0>(*tuple2)), i32 &>));
         hud_assert_true((hud::is_same_v<decltype(hud::get<1>(*tuple2)), i32 &>));
         hud_assert_true((hud::is_same_v<decltype(hud::get<2>(*tuple2)), i32 &>));
@@ -226,8 +225,9 @@ GTEST_TEST(tuple, default_constructor_uninit)
         hud::memory::free(tuple2);
 
         tuple_type3 *tuple3 = hud::memory::allocate_array<tuple_type3>(1);
-        hud::memory::set(tuple3, sizeof(tuple_type3), 0xFF);
-        hud::memory::construct_at(tuple3);
+        // Be sure optimizer don't remove the memmory::set
+        volatile void *do_not_optimize_set_tuple3 = hud::memory::set(tuple3, sizeof(tuple_type3), 0xFF);
+        hud::memory::construct_at(static_cast<volatile tuple_type3 *>(do_not_optimize_set_tuple3));
         hud_assert_true((hud::is_same_v<decltype(hud::get<0>(*tuple3)), i32 &>));
         hud_assert_true((hud::is_same_v<decltype(hud::get<1>(*tuple3)), f32 &>));
         hud_assert_true((hud::is_same_v<decltype(hud::get<2>(*tuple3)), wchar &>));
@@ -245,8 +245,9 @@ GTEST_TEST(tuple, default_constructor_uninit)
         hud::memory::free(tuple3);
 
         tuple_type4 *tuple4 = hud::memory::allocate_array<tuple_type4>(1);
-        hud::memory::set(tuple4, sizeof(tuple_type4), 0xFF);
-        hud::memory::construct_at(tuple4);
+        // Be sure optimizer don't remove the memmory::set
+        volatile void *do_not_optimize_set_tuple4 = hud::memory::set(tuple4, sizeof(tuple_type4), 0xFF);
+        hud::memory::construct_at(static_cast<volatile tuple_type4 *>(do_not_optimize_set_tuple4));
         hud_assert_true((hud::is_same_v<decltype(hud::get<0>(*tuple4)), hud_test::explicit_default_constructible &>));
         hud_assert_true((hud::is_same_v<decltype(hud::get<1>(*tuple4)), hud_test::implicit_default_constructible &>));
         hud_assert_eq((hud::get<0>(*tuple4).id()), hud_test::explicit_default_constructible::DEFAULT_ID_VALUE);
@@ -263,8 +264,9 @@ GTEST_TEST(tuple, default_constructor_uninit)
         hud::memory::free(tuple4);
 
         tuple_type5 *tuple5 = hud::memory::allocate_array<tuple_type5>(1);
-        hud::memory::set(tuple5, sizeof(tuple_type5), 0xFF);
-        hud::memory::construct_at(tuple5);
+        // Be sure optimizer don't remove the memmory::set
+        volatile void *do_not_optimize_set_tuple5 = hud::memory::set(tuple5, sizeof(tuple_type5), 0xFF);
+        hud::memory::construct_at(static_cast<volatile tuple_type5 *>(do_not_optimize_set_tuple5));
         hud_assert_true((hud::is_same_v<decltype(hud::get<0>(*tuple5)), hud_test::explicit_default_constructible &>));
         hud_assert_true((hud::is_same_v<decltype(hud::get<1>(*tuple5)), i32 &>));
         hud_assert_eq((hud::get<0>(*tuple5).id()), hud_test::explicit_default_constructible::DEFAULT_ID_VALUE);
