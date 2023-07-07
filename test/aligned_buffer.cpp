e5712de406344a8fe32d6c69b5e81852ad292ba9
Julian LALU <julian.lalu@outlook.com>
2023-07-07 14:47:43 +0200

    Print concerned alignment

--- test/aligned_buffer.cpp
+++ test/aligned_buffer.cpp
@@ -3,13 +3,13 @@
 GTEST_TEST(aligned_buffer, default_constructor_should_aligned_buffer_correctly)
 {
 
-    using size = std::integer_sequence<int, 1, 2, 3, 4, 5, 6, 7, 8, 9>;
-    using align = std::integer_sequence<int, 1, 2, 4, 8, 16, 32, 64, 128>;
-    hud_test::for_each_value<size>()([]<int size>()
-                                     { hud_test::for_each_value<align>()([]<int alignement>()
+    using size = std::integer_sequence<usize, 1, 2, 3, 4, 5, 6, 7, 8, 9>;
+    using align = std::integer_sequence<u32, 1, 2, 4, 8, 16, 32, 64, 128>;
+    hud_test::for_each_value<size>()([]<usize size>()
+                                     { hud_test::for_each_value<align>()([]<u32 alignement>()
                                                                          {
             hud::aligned_buffer<size, alignement> buffer;
             volatile void* ptr = buffer.pointer();
-            hud_assert_eq(reinterpret_cast<const uptr>(ptr), alignement);
+            printf("Size %lu, Alignement %u\n", size, alignement);
             hud_assert_true(hud::memory::is_pointer_aligned(ptr, alignement)); }); });
 }
