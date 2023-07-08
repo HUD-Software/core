f962291dd526ac79143263a36f71a526984e67f7
HUD-Software <104791429+HUD-Software@users.noreply.github.com>
2023-07-08 10:13:50 +0200

    Update aligned_buffer.cpp

--- test/aligned_buffer.cpp
+++ test/aligned_buffer.cpp
@@ -4,7 +4,7 @@ GTEST_TEST(aligned_buffer, default_constructor_should_aligned_buffer_correctly)
 {
 
     using size = std::integer_sequence<usize, 1, 2, 3, 4, 5, 6, 7, 8, 9>;
-    using align = std::integer_sequence<u32, 1, 2, 4, 8, 16, 32;
+    using align = std::integer_sequence<u32, 1, 2, 4, 8, 16, 32>;
     hud_test::for_each_value<size>()([]<usize size>()
                                      { hud_test::for_each_value<align>()([]<u32 alignement>()
                                                                          {
