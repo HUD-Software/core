b645b2b6b48ee5e3b6dddeb4347c3a5a40418e75
Julian LALU <julian.lalu@outlook.com>
2023-09-12 11:02:45 +0200

    Start coverage GCOV

--- coverage.cmake
+++ coverage.cmake
@@ -59,7 +59,7 @@ if(MSVC)
 			COMMAND Powershell.exe Expand-Archive -Path ./grcov-x86_64-pc-windows-msvc.zip -DestinationPath . -F
 		)
 
-    	add_custom_command( 
+		add_custom_command( 
 		 	TARGET ${project_name} POST_BUILD
 			COMMAND echo Start coverage...
 			COMMAND ${CMAKE_COMMAND} -E env LLVM_PROFILE_FILE="${project_name}.profraw" ./${VS_CONFIG}/${project_name}.exe
@@ -129,45 +129,133 @@ elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
 	target_link_options(${lib_name} PRIVATE -fprofile-instr-generate -fcoverage-mapping)
 	
 	add_custom_command( 
+		TARGET ${project_name} POST_BUILD
+		COMMAND echo Download Grcov...
+		COMMAND curl -L https://github.com/mozilla/grcov/releases/latest/download/grcov-x86_64-unknown-linux-gnu.tar.bz2 | tar jxf -
+	)
+
+	add_custom_command( 
+		TARGET ${project_name} POST_BUILD
+		COMMAND echo Start coverage...
+		COMMAND ${CMAKE_COMMAND} -E env LLVM_PROFILE_FILE="${lib_name}.profraw" ./${project_name}
+	)
+
+	add_custom_command( 
+		TARGET ${project_name} POST_BUILD
+		COMMAND echo Merge coverage info...
+		COMMAND ${CMAKE_CXX_COMPILER_PATH}/llvm-profdata merge ${lib_name}.profraw -o ${lib_name}.profdata
+	)
+
+	add_custom_command( 
+		TARGET ${project_name} POST_BUILD
+		COMMAND echo Show profraw...
+		COMMAND ${CMAKE_CXX_COMPILER_PATH}/llvm-profdata show --all-functions ${lib_name}.profdata >> profraw.info.txt
+	)
+
+	add_custom_command( 
+		TARGET ${project_name} POST_BUILD
+		COMMAND echo Show coverage info...
+		COMMAND rm -f show.txt
+		COMMAND ${CMAKE_CXX_COMPILER_PATH}/llvm-cov show ./${project_name} -instr-profile=${lib_name}.profdata --show-expansions >> show.txt
+	)
+
+
+	add_custom_command( 
+		TARGET ${project_name} POST_BUILD
+		COMMAND echo Generate HTML report...
+		COMMAND rm -f coverage.ubuntu.lcov.info
+		COMMAND ./grcov --llvm -t html -b . -s ./../../
+				--llvm-path /usr/bin/
+				--branch
+				--keep-only "src/*" 
+				--keep-only "interface/*"
+				--excl-start "^.*LCOV_EXCL_START.*" 
+				--excl-stop "^.*LCOV_EXCL_STOP.*" 
+				--excl-line "\"(\\s*^.*GTEST_TEST\\.*)|(^.*LCOV_EXCL_LINE.*)\"" 
+				--excl-br-start "^.*LCOV_EXCL_START.*" 
+				--excl-br-stop "^.*LCOV_EXCL_STOP.*" 
+				--excl-br-line "\"(\\s*^.*GTEST_TEST\\.*)|(^.*LCOV_EXCL_BR_LINE.*)\"" 
+				-o ubuntu
+				..
+	)
+
+	add_custom_command( 
+		TARGET ${project_name} POST_BUILD
+		COMMAND echo Generate LCOV report...
+		COMMAND rm -f coverage.ubuntu.lcov.info
+		COMMAND ./grcov --llvm -t lcov -b . -s ./../../
+				--llvm-path /usr/bin/
+				--branch
+				--keep-only "src/*"
+				--keep-only "interface/*"
+				--excl-start "^.*LCOV_EXCL_START.*" 
+				--excl-stop "^.*LCOV_EXCL_STOP.*" 
+				--excl-line "\"(\\s*^.*GTEST_TEST\\.*)|(^.*LCOV_EXCL_LINE.*)\"" 
+				--excl-br-start "^.*LCOV_EXCL_START.*" 
+				--excl-br-stop "^.*LCOV_EXCL_STOP.*" 
+				--excl-br-line "\"(\\s*^.*GTEST_TEST\\.*)|(^.*LCOV_EXCL_BR_LINE.*)\"" 
+				-o coverage.ubuntu.lcov.info
+				..
+	)
+endif()
+endfunction()
+
+function(enable_gcov_coverage project_name lib_name)
+if(MSVC)
+	if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+		set(MSVC_CODECOVERAGE_CONSOLE_PATH "C:\\Program Files\\Microsoft Visual Studio\\2022\\Enterprise\\Common7\\IDE\\Extensions\\Microsoft\\CodeCoverage.Console\\Microsoft.CodeCoverage.Console.exe" CACHE STRING "Path to Microsoft.CodeCoverage.Console.exe")
+		find_program(MSVC_CODECOVERAGE_CONSOLE_EXE ${MSVC_CODECOVERAGE_CONSOLE_PATH})
+		if(NOT MSVC_CODECOVERAGE_CONSOLE_EXE)
+			message(FATAL_ERROR "Code coverage on Windows need Microsoft.CodeCoverage.Console.exe available in Visual Studio 2022 17.3 Enterprise Edition")
+		endif()
+
+		target_link_options(${project_name} PRIVATE /PROFILE)
+		
+		add_custom_command(
+			TARGET ${project_name} POST_BUILD
+			COMMENT "Instrument and Collect ${project_name}.exe"
+			COMMAND ${MSVC_CODECOVERAGE_CONSOLE_EXE} instrument Release\\${project_name}.exe -s ..\\..\\test\\coverage.runsettings
+			COMMAND ${MSVC_CODECOVERAGE_CONSOLE_EXE} collect Release\\${project_name}.exe -o Release\\coverage.msvc -f cobertura -s ..\\..\\test\\coverage.runsettings
+		)
+	elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+		# Disable compiler batching to fix a clang-cl bug when activate --coverage
+		# See: https://developercommunity.visualstudio.com/t/Clang-cl---coverage-option-create-gcno-w/10253777
+		set_property(TARGET test_core PROPERTY VS_NO_COMPILE_BATCHING ON)
+		set_property(TARGET core PROPERTY VS_NO_COMPILE_BATCHING ON)
+
+		target_compile_options(${project_name} PRIVATE --coverage)
+		#target_link_options(${project_name} PRIVATE --coverage)
+		target_compile_options(${lib_name} PRIVATE --coverage)
+		#target_link_options(${lib_name} PRIVATE --coverage)
+
+		# Add clang lib path to libraries paths
+		get_filename_component(CMAKE_CXX_COMPILER_PATH ${CMAKE_CXX_COMPILER} DIRECTORY)
+		target_link_directories(${project_name} PRIVATE "${CMAKE_CXX_COMPILER_PATH}\\..\\lib\\clang\\${CMAKE_CXX_COMPILER_PATH}\\lib\\windows\\")
+		message("${CMAKE_CXX_COMPILER_PATH}\\..\\lib\\clang\\${CMAKE_CXX_COMPILER_PATH}\\lib\\windows\\")
+		# Need to link manually LLVM Bug 40877
+		# See: https://bugs.llvm.org/show_bug.cgi?id=40877
+		target_link_libraries(test_core PRIVATE "clang_rt.profile-x86_64.lib")
+
+
+		add_custom_command( 
 		 	TARGET ${project_name} POST_BUILD
 			COMMAND echo Download Grcov...
-			COMMAND curl -L https://github.com/mozilla/grcov/releases/latest/download/grcov-x86_64-unknown-linux-gnu.tar.bz2 | tar jxf -
+			COMMAND Powershell.exe Invoke-WebRequest -Uri https://github.com/mozilla/grcov/releases/download/v0.8.13/grcov-x86_64-pc-windows-msvc.zip -OutFile ./grcov-x86_64-pc-windows-msvc.zip
+			COMMAND Powershell.exe Expand-Archive -Path ./grcov-x86_64-pc-windows-msvc.zip -DestinationPath . -F
 		)
 
-    	add_custom_command( 
+		add_custom_command( 
 		 	TARGET ${project_name} POST_BUILD
 			COMMAND echo Start coverage...
-			COMMAND ${CMAKE_COMMAND} -E env LLVM_PROFILE_FILE="${lib_name}.profraw" ./${project_name}
+			COMMAND ./${VS_CONFIG}/${project_name}.exe
 		)
-
-		add_custom_command( 
-			TARGET ${project_name} POST_BUILD
-			COMMAND echo Merge coverage info...
-			COMMAND ${CMAKE_CXX_COMPILER_PATH}/llvm-profdata merge ${lib_name}.profraw -o ${lib_name}.profdata
-		)
-
-		add_custom_command( 
-			TARGET ${project_name} POST_BUILD
-			COMMAND echo Show profraw...
-			COMMAND ${CMAKE_CXX_COMPILER_PATH}/llvm-profdata show --all-functions ${lib_name}.profdata >> profraw.info.txt
-		)
-
-		add_custom_command( 
-			TARGET ${project_name} POST_BUILD
-			COMMAND echo Show coverage info...
-			COMMAND rm -f show.txt
-			COMMAND ${CMAKE_CXX_COMPILER_PATH}/llvm-cov show ./${project_name} -instr-profile=${lib_name}.profdata --show-expansions >> show.txt
-		)
-
 
 		add_custom_command( 
 			TARGET ${project_name} POST_BUILD
 			COMMAND echo Generate HTML report...
-			COMMAND rm -f coverage.ubuntu.lcov.info
-			COMMAND ./grcov --llvm -t html -b . -s ./../../
-					--llvm-path /usr/bin/
-					--guess-directory-when-missing
-					--branch
+			COMMAND ./grcov.exe --llvm -t html -b ./${VS_CONFIG}/ -s ./../../
+					--llvm-path ${CMAKE_CXX_COMPILER_PATH}
+					#--branch
 					--keep-only "src/*" 
 					--keep-only "interface/*"
 					--excl-start "^.*LCOV_EXCL_START.*" 
@@ -176,17 +264,16 @@ elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
 					--excl-br-start "^.*LCOV_EXCL_START.*" 
 					--excl-br-stop "^.*LCOV_EXCL_STOP.*" 
 					--excl-br-line "\"(\\s*^.*GTEST_TEST\\.*)|(^.*LCOV_EXCL_BR_LINE.*)\"" 
-					-o ubuntu
+					-o windows
 					..
 		)
 
 		add_custom_command( 
 			TARGET ${project_name} POST_BUILD
 			COMMAND echo Generate LCOV report...
-			COMMAND rm -f coverage.ubuntu.lcov.info
-			COMMAND ./grcov --llvm -t lcov -b . -s ./../../
-					--llvm-path /usr/bin/
-					--branch
+			COMMAND ./grcov.exe --llvm -t lcov -b ./${VS_CONFIG}/ -s ./../../
+					--llvm-path ${CMAKE_CXX_COMPILER_PATH}
+					#--branch
 					--keep-only "src/*"
 					--keep-only "interface/*"
 					--excl-start "^.*LCOV_EXCL_START.*" 
@@ -195,27 +282,69 @@ elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
 					--excl-br-start "^.*LCOV_EXCL_START.*" 
 					--excl-br-stop "^.*LCOV_EXCL_STOP.*" 
 					--excl-br-line "\"(\\s*^.*GTEST_TEST\\.*)|(^.*LCOV_EXCL_BR_LINE.*)\"" 
-					-o coverage.ubuntu.lcov.info
+					-o coverage.windows.lcov.info
 					..
 		)
-endif()
-endfunction()
-
-function(enable_gcov_coverage project_name lib_name)
-if(MSVC)
-	if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
-		message(FATAL_ERROR "Gcov coverage is not supported with MSVC compiler")
-	elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-		target_compile_options(${project_name} PRIVATE --coverage)
-		target_compile_options(${lib_name} PRIVATE --coverage)
-		target_link_options(${project_name} PRIVATE --coverage)
-		#target_link_options(${lib_name} PRIVATE --coverage)
-
-		# Add clang lib path to libraries paths
-		get_filename_component(CMAKE_CXX_COMPILER_PATH ${CMAKE_CXX_COMPILER} DIRECTORY)
-		target_link_directories(${project_name} PRIVATE "${CMAKE_CXX_COMPILER_PATH}\\..\\lib\\clang\\${CMAKE_CXX_COMPILER_VERSION}\\lib\\windows\\")
-		target_link_libraries(${project_name} INTERFACE clang_rt.profile-x86_64.lib)
 
 	endif()
+elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")*
+	# Disable compiler batching to fix a clang-cl bug when activate --coverage
+	# See: https://developercommunity.visualstudio.com/t/Clang-cl---coverage-option-create-gcno-w/10253777
+	set_property(TARGET test_core PROPERTY VS_NO_COMPILE_BATCHING ON)
+	set_property(TARGET core PROPERTY VS_NO_COMPILE_BATCHING ON)
+
+	target_compile_options(${project_name} PRIVATE --coverage)
+	#target_link_options(${project_name} PRIVATE --coverage)
+	target_compile_options(${lib_name} PRIVATE --coverage)
+	#target_link_options(${lib_name} PRIVATE --coverage)
+
+
+	add_custom_command( 
+		TARGET ${project_name} POST_BUILD
+		COMMAND echo Download Grcov...
+		COMMAND curl -L https://github.com/mozilla/grcov/releases/latest/download/grcov-x86_64-unknown-linux-gnu.tar.bz2 | tar jxf -
+	)
+
+	add_custom_command( 
+		TARGET ${project_name} POST_BUILD
+		COMMAND echo Start coverage...
+		COMMAND ./${VS_CONFIG}/${project_name}.exe
+	)
+
+	add_custom_command( 
+		TARGET ${project_name} POST_BUILD
+		COMMAND echo Generate HTML report...
+		COMMAND ./grcov --llvm -t html -b . -s ./../../
+				--llvm-path /usr/bin/
+				--branch
+				--keep-only "src/*" 
+				--keep-only "interface/*"
+				--excl-start "^.*LCOV_EXCL_START.*" 
+				--excl-stop "^.*LCOV_EXCL_STOP.*" 
+				--excl-line "\"(\\s*^.*GTEST_TEST\\.*)|(^.*LCOV_EXCL_LINE.*)\"" 
+				--excl-br-start "^.*LCOV_EXCL_START.*" 
+				--excl-br-stop "^.*LCOV_EXCL_STOP.*" 
+				--excl-br-line "\"(\\s*^.*GTEST_TEST\\.*)|(^.*LCOV_EXCL_BR_LINE.*)\"" 
+				-o ubuntu
+				..
+	)
+
+	add_custom_command( 
+		TARGET ${project_name} POST_BUILD
+		COMMAND echo Generate LCOV report...
+		COMMAND ./grcov --llvm -t lcov -b . -s ./../../
+				--llvm-path /usr/bin/
+				--branch
+				--keep-only "src/*"
+				--keep-only "interface/*"
+				--excl-start "^.*LCOV_EXCL_START.*" 
+				--excl-stop "^.*LCOV_EXCL_STOP.*" 
+				--excl-line "\"(\\s*^.*GTEST_TEST\\.*)|(^.*LCOV_EXCL_LINE.*)\"" 
+				--excl-br-start "^.*LCOV_EXCL_START.*" 
+				--excl-br-stop "^.*LCOV_EXCL_STOP.*" 
+				--excl-br-line "\"(\\s*^.*GTEST_TEST\\.*)|(^.*LCOV_EXCL_BR_LINE.*)\"" 
+				-o coverage.ubuntu.lcov.info
+				..
+	)
 endif()
 endfunction()
