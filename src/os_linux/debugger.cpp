b675a9ac6cc56cb79540bb9e4d44711cb74dcbc9
HUDSoftware <hudsoftware@outlook.fr>
2024-09-26 12:10:00 +0200

    Update coverage

--- src/os_linux/debugger.cpp
+++ src/os_linux/debugger.cpp
@@ -13,25 +13,30 @@ namespace hud::os::linux
         // https://man7.org/linux/man-pages/man5/procfs.5.html
         i32 file_descriptor = open("/proc/self/status", O_RDONLY);
         if (file_descriptor == -1)
+        // LCOV_EXCL_START
         {
-            return false; // LCOV_EXCL_LINE
+            return false;
         }
+        // LCOV_EXCL_STOP
 
         constexpr usize BUFFER_BYTES_COUNT = 256;
         ansichar buffer[BUFFER_BYTES_COUNT];
         isize read_count = read(file_descriptor, buffer, BUFFER_BYTES_COUNT);
+        // LCOV_EXCL_START
         if (read_count == -1)
         {
-            return false; // LCOV_EXCL_LINE
+            return false;
         }
-
+        // LCOV_EXCL_STOP
         buffer[read_count - 1] = '\0';
         constexpr ansichar TRACERPID[] = "TracerPid:\t";
         ansichar *found = strstr(buffer, TRACERPID);
         if (found == nullptr)
+        // LCOV_EXCL_START
         {
-            return false; // LCOV_EXCL_LINE
+            return false;
         }
+        // LCOV_EXCL_STOP
         return found[sizeof(TRACERPID) - 1] != '0';
     }
 } // namespace hud::os::linux
