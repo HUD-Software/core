782482afbe42cb254cd217dca7207834fb62883f
Julian LALU <julian.lalu@outlook.fr>
2024-10-01 16:03:09 +0200

    Add missing tests

--- test/i128/i128_operations.cpp
+++ test/i128/i128_operations.cpp
@@ -9,12 +9,38 @@ GTEST_TEST(i128, addition)
 #endif
 }
 
+GTEST_TEST(i128, addition_equal)
+{
+    hud::i128 a {1};
+    a += hud::i128 {2};
+    hud_assert_eq(a, hud::i128 {3});
+// Check for overflow of i128; works if it's a two's complement representation.
+#if !USE_ADDRESS_SANITIZER
+    hud::i128 b {1};
+    b += hud::i128_max;
+    hud_assert_eq(b, hud::i128_min); // Signed integer overflow is undefined behaviour
+#endif
+}
+
 GTEST_TEST(i128, subtraction)
 {
     hud_assert_eq(hud::i128 {3} - hud::i128 {2}, hud::i128 {1});
 // Check for underflow of i128; works if it's a two's complement representation.
 #if !USE_ADDRESS_SANITIZER
     hud_assert_eq(hud::i128_min - hud::i128 {1}, hud::i128_max); // Signed integer overflow is undefined behaviour
+#endif
+}
+
+GTEST_TEST(i128, subtraction_equal)
+{
+    hud::i128 a {3};
+    a -= hud::i128 {2};
+    hud_assert_eq(a, hud::i128 {1});
+// Check for underflow of i128; works if it's a two's complement representation.
+#if !USE_ADDRESS_SANITIZER
+    hud::i128 b {hud::i128_min};
+    b -= hud::i128 {1};
+    hud_assert_eq(b, hud::i128_max); // Signed integer overflow is undefined behaviour
 #endif
 }
 
@@ -31,6 +57,35 @@ GTEST_TEST(i128, division)
     hud_assert_eq((hud::i128 {0x7FFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF} / hud::i128 {0, 3}), (hud::i128 {0x2AAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAAAA}));
 }
 
+GTEST_TEST(i128, division_equal)
+{
+    hud::i128 a {2};
+    a /= hud::i128 {2};
+    hud_assert_eq(a, hud::i128 {1});
+    hud::i128 b {3};
+    b /= hud::i128 {2};
+    hud_assert_eq(b, hud::i128 {1});
+    hud::i128 c {10};
+    c /= hud::i128 {2};
+    hud_assert_eq(c, hud::i128 {5});
+    hud::i128 d {-6};
+    d /= hud::i128 {3};
+    hud_assert_eq(d, hud::i128 {-2});
+    hud::i128 e {9};
+    e /= hud::i128 {3};
+    hud_assert_eq(e, hud::i128 {3});
+
+    hud::i128 f {0x7FFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF};
+    f /= hud::i128 {0, 2};
+    hud_assert_eq(f, (hud::i128 {0x3FFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF}));
+    hud::i128 g {(i64)0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF};
+    g /= hud::i128 {0, 1};
+    hud_assert_eq(g, (hud::i128 {(i64)0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF}));
+    hud::i128 h {0x7FFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF};
+    h /= hud::i128 {0, 3};
+    hud_assert_eq(h, (hud::i128 {0x2AAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAAAA}));
+}
+
 GTEST_TEST(i128, multiplication)
 {
     hud_assert_eq(hud::i128 {2} * hud::i128 {2}, hud::i128 {4});
@@ -45,6 +100,34 @@ GTEST_TEST(i128, multiplication)
 #endif
 }
 
+GTEST_TEST(i128, multiplication_equal)
+{
+    hud::i128 a {2};
+    a *= hud::i128 {2};
+    hud_assert_eq(a, hud::i128 {4});
+    hud::i128 b {3};
+    b *= hud::i128 {0};
+    hud_assert_eq(b, hud::i128 {0});
+    hud::i128 c {3};
+    c *= hud::i128 {2};
+    hud_assert_eq(c, hud::i128 {6});
+    hud::i128 d {0};
+    d *= hud::i128 {10};
+    hud_assert_eq(d, hud::i128 {0});
+    hud::i128 e {-3};
+    e *= hud::i128 {4};
+    hud_assert_eq(e, hud::i128 {-12});
+    hud::i128 f {7};
+    f *= hud::i128 {5};
+    hud_assert_eq(f, hud::i128 {35});
+// Check for overflow of i128; works if it's a two's complement representation.
+#if !USE_ADDRESS_SANITIZER
+    hud::i128 g {hud::i128_max};
+    g *= hud::i128 {2};
+    hud_assert_eq(g, i128 {-2}); // Signed integer overflow is undefined behaviour
+#endif
+}
+
 GTEST_TEST(i128, modulus)
 {
     hud_assert_eq(hud::i128 {2} % hud::i128 {2}, hud::i128 {0});
@@ -53,6 +136,26 @@ GTEST_TEST(i128, modulus)
 
     hud_assert_eq(hud::i128 {0} % hud::i128_max, hud::i128 {0});
     hud_assert_eq(hud::i128_max % hud::i128_max, hud::i128 {0});
+}
+
+GTEST_TEST(i128, modulus_equal)
+{
+    hud::i128 a {2};
+    a %= hud::i128 {2};
+    hud_assert_eq(a, hud::i128 {0});
+    hud::i128 b {3};
+    b %= hud::i128 {2};
+    hud_assert_eq(b, hud::i128 {1});
+    hud::i128 c {10};
+    c %= hud::i128 {4};
+    hud_assert_eq(c, hud::i128 {2});
+    hud::i128 d {0};
+    d %= hud::i128 {hud::i128_max};
+    hud_assert_eq(d, hud::i128 {0});
+
+    hud::i128 e {hud::i128_max};
+    e %= hud::i128 {hud::i128_max};
+    hud_assert_eq(e, hud::i128 {0});
 }
 
 GTEST_TEST(i128, negate)
@@ -85,9 +188,15 @@ GTEST_TEST(i128, logical_or)
 
 GTEST_TEST(i128, logical_and)
 {
-    hud_assert_eq(hud::i128 {0} & hud::i128 {1}, hud::i128 {0});
-    hud_assert_eq(hud::i128 {2} & hud::i128 {3}, hud::i128 {2});
-    hud_assert_eq(hud::i128 {0} & hud::i128_max, hud::i128 {0});
+    hud::i128 a {0};
+    a &= hud::i128 {1};
+    hud_assert_eq(a, hud::i128 {0});
+    hud::i128 b {2};
+    b &= hud::i128 {3};
+    hud_assert_eq(b, hud::i128 {2});
+    hud::i128 c {0};
+    c &= hud::i128 {hud::i128_max};
+    hud_assert_eq(c, hud::i128 {0});
 }
 
 GTEST_TEST(i128, logical_xor)
@@ -100,17 +209,15 @@ GTEST_TEST(i128, logical_xor)
 
 GTEST_TEST(i128, logical_or_equal)
 {
-    hud::i128 v0 {0};
-    v0 |= hud::i128 {1};
-    hud_assert_eq(v0, hud::i128 {1});
-
-    hud::i128 v1 {2};
-    v1 |= hud::i128 {1};
-    hud_assert_eq(v1, hud::i128 {3});
-
-    hud::i128 v2 {0};
-    v2 |= hud::i128_max;
-    hud_assert_eq(v2, hud::i128_max);
+    hud::i128 a {0};
+    a |= hud::i128 {1};
+    hud_assert_eq(a, hud::i128 {1});
+    hud::i128 b {2};
+    b |= hud::i128 {1};
+    hud_assert_eq(b, hud::i128 {3});
+    hud::i128 c {0};
+    c |= hud::i128_max;
+    hud_assert_eq(c, hud::i128_max);
 }
 
 GTEST_TEST(i128, logical_and_equal)
--- test/u128/u128_operations.cpp
+++ test/u128/u128_operations.cpp
@@ -10,23 +10,103 @@ GTEST_TEST(u128, addition)
 GTEST_TEST(u128, subtraction)
 {
     hud_assert_eq(hud::u128 {3} - hud::u128 {2}, hud::u128 {1});
-
     // Check for underflow of u128; works if it's a two's complement representation.
     hud_assert_eq(hud::u128_min - hud::u128 {1}, hud::u128_max);
+}
+
+GTEST_TEST(u128, subtraction_equal)
+{
+    hud::u128 a {3};
+    a -= hud::u128 {2};
+    hud_assert_eq(a, hud::u128 {1});
+// Check for underflow of u128; works if it's a two's complement representation.
+#if !USE_ADDRESS_SANITIZER
+    hud::u128 b {hud::u128_min};
+    b -= hud::u128 {1};
+    hud_assert_eq(b, hud::u128_max); // Signed integer overflow is undefined behaviour
+#endif
 }
 
 GTEST_TEST(u128, division)
 {
     hud_assert_eq(hud::u128 {2} / hud::u128 {2}, hud::u128 {1});
     hud_assert_eq(hud::u128 {3} / hud::u128 {2}, hud::u128 {1});
+    hud_assert_eq(hud::u128 {10} / hud::u128 {2}, hud::u128 {5});
+    hud_assert_eq(hud::u128 {-6} / hud::u128 {3}, (hud::u128 {0x5555555555555555, 0x5555555555555553}));
+    hud_assert_eq(hud::u128 {9} / hud::u128 {3}, hud::u128 {3});
+
+    hud_assert_eq((hud::u128 {0x7FFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF} / hud::u128 {0, 2}), (hud::u128 {0x3FFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF}));
+    hud_assert_eq((hud::u128 {0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF} / hud::u128 {0, 1}), (hud::u128 {0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF}));
+    hud_assert_eq((hud::u128 {0x7FFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF} / hud::u128 {0, 3}), (hud::u128 {0x2AAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAAAA}));
+}
+
+GTEST_TEST(u128, division_equal)
+{
+    hud::u128 a {2};
+    a /= hud::u128 {2};
+    hud_assert_eq(a, hud::u128 {1});
+    hud::u128 b {3};
+    b /= hud::u128 {2};
+    hud_assert_eq(b, hud::u128 {1});
+    hud::u128 c {10};
+    c /= hud::u128 {2};
+    hud_assert_eq(c, hud::u128 {5});
+    hud::u128 d {-6};
+    d /= hud::u128 {3};
+    hud_assert_eq(d, (hud::u128 {0x5555555555555555, 0x5555555555555553}));
+    hud::u128 e {9};
+    e /= hud::u128 {3};
+    hud_assert_eq(e, hud::u128 {3});
+
+    hud::u128 f {0x7FFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF};
+    f /= hud::u128 {0, 2};
+    hud_assert_eq(f, (hud::u128 {0x3FFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF}));
+    hud::u128 g {0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF};
+    g /= hud::u128 {0, 1};
+    hud_assert_eq(g, (hud::u128 {0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF}));
+    hud::u128 h {0x7FFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF};
+    h /= hud::u128 {0, 3};
+    hud_assert_eq(h, (hud::u128 {0x2AAAAAAAAAAAAAAA, 0xAAAAAAAAAAAAAAAA}));
 }
 
 GTEST_TEST(u128, multiplication)
 {
     hud_assert_eq(hud::u128 {2} * hud::u128 {2}, hud::u128 {4});
     hud_assert_eq(hud::u128 {3} * hud::u128 {0}, hud::u128 {0});
+    hud_assert_eq(hud::u128 {3} * hud::u128 {2}, hud::u128 {6});
+    hud_assert_eq(hud::u128 {0} * hud::u128 {10}, hud::u128 {0});
+    hud_assert_eq(hud::u128 {-3} * hud::u128 {4}, hud::u128 {-12});
+    hud_assert_eq(hud::u128 {7} * hud::u128 {5}, hud::u128 {35});
     // Check for overflow of u128; works if it's a two's complement representation.
     hud_assert_eq(hud::u128_max * hud::u128 {2}, hud::u128_max - 1);
+}
+
+GTEST_TEST(u128, multiplication_equal)
+{
+    hud::u128 a {2};
+    a *= hud::u128 {2};
+    hud_assert_eq(a, hud::u128 {4});
+    hud::u128 b {3};
+    b *= hud::u128 {0};
+    hud_assert_eq(b, hud::u128 {0});
+    hud::u128 c {3};
+    c *= hud::u128 {2};
+    hud_assert_eq(c, hud::u128 {6});
+    hud::u128 d {0};
+    d *= hud::u128 {10};
+    hud_assert_eq(d, hud::u128 {0});
+    hud::u128 e {-3};
+    e *= hud::u128 {4};
+    hud_assert_eq(e, hud::u128 {-12});
+    hud::u128 f {7};
+    f *= hud::u128 {5};
+    hud_assert_eq(f, hud::u128 {35});
+// Check for overflow of u128; works if it's a two's complement representation.
+#if !USE_ADDRESS_SANITIZER
+    hud::u128 g {hud::u128_max};
+    g *= hud::u128 {2};
+    hud_assert_eq(g, hud::u128_max - 1); // Signed integer overflow is undefined behaviour
+#endif
 }
 
 GTEST_TEST(u128, modulus)
@@ -39,10 +119,31 @@ GTEST_TEST(u128, modulus)
     hud_assert_eq(hud::u128_max % hud::u128_max, hud::u128 {0});
 }
 
+GTEST_TEST(u128, modulus_equal)
+{
+    hud::u128 a {2};
+    a %= hud::u128 {2};
+    hud_assert_eq(a, hud::u128 {0});
+    hud::u128 b {3};
+    b %= hud::u128 {2};
+    hud_assert_eq(b, hud::u128 {1});
+    hud::u128 c {10};
+    c %= hud::u128 {4};
+    hud_assert_eq(c, hud::u128 {2});
+    hud::u128 d {0};
+    d %= hud::u128 {hud::u128_max};
+    hud_assert_eq(d, hud::u128 {0});
+
+    hud::u128 e {hud::u128_max};
+    e %= hud::u128 {hud::u128_max};
+    hud_assert_eq(e, hud::u128 {0});
+}
+
 GTEST_TEST(u128, negate)
 {
     hud_assert_eq(-hud::u128 {0}, hud::u128 {0});
-    hud_assert_eq(-hud::u128 {2}, hud::u128_max - 1);
+    hud_assert_eq(-hud::u128 {2}, hud::u128 {-2});
+    hud_assert_eq(-hud::u128 {-2}, hud::u128 {2});
 }
 
 GTEST_TEST(u128, not )
@@ -68,9 +169,15 @@ GTEST_TEST(u128, logical_or)
 
 GTEST_TEST(u128, logical_and)
 {
-    hud_assert_eq(hud::u128 {0} & hud::u128 {1}, hud::u128 {0});
-    hud_assert_eq(hud::u128 {2} & hud::u128 {3}, hud::u128 {2});
-    hud_assert_eq(hud::u128 {0} & hud::u128_max, hud::u128 {0});
+    hud::u128 a {0};
+    a &= hud::u128 {1};
+    hud_assert_eq(a, hud::u128 {0});
+    hud::u128 b {2};
+    b &= hud::u128 {3};
+    hud_assert_eq(b, hud::u128 {2});
+    hud::u128 c {0};
+    c &= hud::u128 {hud::u128_max};
+    hud_assert_eq(c, hud::u128 {0});
 }
 
 GTEST_TEST(u128, logical_xor)
@@ -83,17 +190,15 @@ GTEST_TEST(u128, logical_xor)
 
 GTEST_TEST(u128, logical_or_equal)
 {
-    hud::u128 v0 {0};
-    v0 |= hud::u128 {1};
-    hud_assert_eq(v0, hud::u128 {1});
-
-    hud::u128 v1 {2};
-    v1 |= hud::u128 {1};
-    hud_assert_eq(v1, hud::u128 {3});
-
-    hud::u128 v2 {0};
-    v2 |= hud::u128_max;
-    hud_assert_eq(v2, hud::u128_max);
+    hud::u128 a {0};
+    a |= hud::u128 {1};
+    hud_assert_eq(a, hud::u128 {1});
+    hud::u128 b {2};
+    b |= hud::u128 {1};
+    hud_assert_eq(b, hud::u128 {3});
+    hud::u128 c {0};
+    c |= hud::u128_max;
+    hud_assert_eq(c, hud::u128_max);
 }
 
 GTEST_TEST(u128, logical_and_equal)
