60f9b1f617900a4d08828d3d711fbc7a2df9a54a
Julian LALU <julian.lalu@outlook.com>
2024-10-06 15:19:26 +0200

    Improve coverage

--- test/memory/memory_compare.cpp
+++ test/memory/memory_compare.cpp
@@ -14,7 +14,8 @@ GTEST_TEST(memory, compare_pointer_to_buffer)
             hud::memory::compare(lhs, rhs1, 3 * sizeof(u8)),
             hud::memory::compare(lhs, rhs2, 3 * sizeof(u8)),
             hud::memory::compare(lhs, rhs3, 3 * sizeof(u8)),
-            hud::memory::compare(lhs, rhs4, 3 * sizeof(u8))};
+            hud::memory::compare(lhs, rhs4, 3 * sizeof(u8))
+        };
     };
 
     // Non constant
@@ -86,7 +87,8 @@ GTEST_TEST(memory, compare_C_buffer)
             hud::memory::compare(lhs, rhs1),
             hud::memory::compare(lhs, rhs2),
             hud::memory::compare(lhs, rhs3),
-            hud::memory::compare(lhs, rhs4)};
+            hud::memory::compare(lhs, rhs4)
+        };
     };
 
     // Non constant
@@ -158,7 +160,8 @@ GTEST_TEST(memory, compare_equal_pointer_to_buffer)
             hud::memory::compare_equal(lhs, rhs1, 3 * sizeof(u8)),
             hud::memory::compare_equal(lhs, rhs2, 3 * sizeof(u8)),
             hud::memory::compare_equal(lhs, rhs3, 3 * sizeof(u8)),
-            hud::memory::compare_equal(lhs, rhs4, 3 * sizeof(u8))};
+            hud::memory::compare_equal(lhs, rhs4, 3 * sizeof(u8))
+        };
     };
 
     // Non constant
@@ -230,7 +233,8 @@ GTEST_TEST(memory, compare_equal_C_buffer)
             hud::memory::compare_equal(lhs, rhs1),
             hud::memory::compare_equal(lhs, rhs2),
             hud::memory::compare_equal(lhs, rhs3),
-            hud::memory::compare_equal(lhs, rhs4)};
+            hud::memory::compare_equal(lhs, rhs4)
+        };
     };
 
     // Non constant
@@ -302,7 +306,8 @@ GTEST_TEST(memory, compare_less_pointer_to_buffer)
             hud::memory::compare_less(lhs, rhs1, 3 * sizeof(u8)),
             hud::memory::compare_less(lhs, rhs2, 3 * sizeof(u8)),
             hud::memory::compare_less(lhs, rhs3, 3 * sizeof(u8)),
-            hud::memory::compare_less(lhs, rhs4, 3 * sizeof(u8))};
+            hud::memory::compare_less(lhs, rhs4, 3 * sizeof(u8))
+        };
     };
 
     // Non constant
@@ -374,7 +379,8 @@ GTEST_TEST(memory, compare_less_c_array)
             hud::memory::compare_less(lhs, rhs1),
             hud::memory::compare_less(lhs, rhs2),
             hud::memory::compare_less(lhs, rhs3),
-            hud::memory::compare_less(lhs, rhs4)};
+            hud::memory::compare_less(lhs, rhs4)
+        };
     };
 
     // Non constant
@@ -446,7 +452,8 @@ GTEST_TEST(memory, compare_greater_pointer_to_buffer)
             hud::memory::compare_greater(lhs, rhs1, 3 * sizeof(u8)),
             hud::memory::compare_greater(lhs, rhs2, 3 * sizeof(u8)),
             hud::memory::compare_greater(lhs, rhs3, 3 * sizeof(u8)),
-            hud::memory::compare_greater(lhs, rhs4, 3 * sizeof(u8))};
+            hud::memory::compare_greater(lhs, rhs4, 3 * sizeof(u8))
+        };
     };
 
     // Non constant
@@ -518,7 +525,8 @@ GTEST_TEST(memory, compare_greater_c_array)
             hud::memory::compare_greater(lhs, rhs1),
             hud::memory::compare_greater(lhs, rhs2),
             hud::memory::compare_greater(lhs, rhs3),
-            hud::memory::compare_greater(lhs, rhs4)};
+            hud::memory::compare_greater(lhs, rhs4)
+        };
     };
 
     // Non constant
@@ -569,6 +577,52 @@ GTEST_TEST(memory, compare_greater_c_array)
         hud_assert_false(std::get<3>(result_buf_3));
 
         constexpr std::tuple<bool, bool, bool, bool> result_buf_4 = test(buf_4, buf, buf_2, buf_3, buf_4);
+        hud_assert_true(std::get<0>(result_buf_4));
+        hud_assert_true(std::get<1>(result_buf_4));
+        hud_assert_true(std::get<2>(result_buf_4));
+        hud_assert_false(std::get<3>(result_buf_4));
+    }
+}
+
+GTEST_TEST(memory, compare_greater_void_pointer_to_buffer)
+{
+
+    constexpr u8 buf[3] = {2, 4, 8};
+    constexpr u8 buf_2[3] = {2, 4, 7};
+    constexpr u8 buf_3[3] = {2, 4, 8};
+    constexpr u8 buf_4[3] = {2, 4, 9};
+
+    auto test = [](const u8 *lhs, const u8 *rhs1, const u8 *rhs2, const u8 *rhs3, const u8 *rhs4) -> std::tuple<bool, bool, bool, bool>
+    {
+        return {
+            hud::memory::compare_greater(static_cast<const void *>(lhs), rhs1, 3 * sizeof(u8)),
+            hud::memory::compare_greater(static_cast<const void *>(lhs), rhs2, 3 * sizeof(u8)),
+            hud::memory::compare_greater(static_cast<const void *>(lhs), rhs3, 3 * sizeof(u8)),
+            hud::memory::compare_greater(static_cast<const void *>(lhs), rhs4, 3 * sizeof(u8))
+        };
+    };
+
+    // Non constant
+    {
+        std::tuple<bool, bool, bool, bool> result_buf = test(buf, buf, buf_2, buf_3, buf_4);
+        hud_assert_false(std::get<0>(result_buf));
+        hud_assert_true(std::get<1>(result_buf));
+        hud_assert_false(std::get<2>(result_buf));
+        hud_assert_false(std::get<3>(result_buf));
+
+        std::tuple<bool, bool, bool, bool> result_buf_2 = test(buf_2, buf, buf_2, buf_3, buf_4);
+        hud_assert_false(std::get<0>(result_buf_2));
+        hud_assert_false(std::get<1>(result_buf_2));
+        hud_assert_false(std::get<2>(result_buf_2));
+        hud_assert_false(std::get<3>(result_buf_2));
+
+        std::tuple<bool, bool, bool, bool> result_buf_3 = test(buf_3, buf, buf_2, buf_3, buf_4);
+        hud_assert_false(std::get<0>(result_buf_3));
+        hud_assert_true(std::get<1>(result_buf_3));
+        hud_assert_false(std::get<2>(result_buf_3));
+        hud_assert_false(std::get<3>(result_buf_3));
+
+        std::tuple<bool, bool, bool, bool> result_buf_4 = test(buf_4, buf, buf_2, buf_3, buf_4);
         hud_assert_true(std::get<0>(result_buf_4));
         hud_assert_true(std::get<1>(result_buf_4));
         hud_assert_true(std::get<2>(result_buf_4));
