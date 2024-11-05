83309bf2b065f5d70cc1780efdced4ab92867f57
Julian LALU <julian.lalu@outlook.com>
2024-11-05 15:16:08 +0100

    Update CRC32

--- interface/core/hash/crc32.h
+++ interface/core/hash/crc32.h
@@ -8,6 +8,26 @@ namespace hud::hash_algorithm
     {
 
         /**
+         * CRC-32 polynomial
+         * Use by ( Source : Wikipedia https://en.wikipedia.org/wiki/Cyclic_redundancy_check#Commonly_used_and_standardized_CRCs ) :
+         * ISO 3309 (HDLC), ANSI X3.66 (ADCCP), FIPS PUB 71, FED-STD-1003, ITU-type_t V.42, ISO/IEC/IEEE 802-3 (Ethernet), SATA, MPEG-2, PKZIP, Gzip, Bzip2, POSIX cksum,PNG, ZMODEM, many others
+         */
+        static constexpr u32 POLYNOMIAL = 0x04c11db7;
+
+        /**
+         * CRC-32 reversed polynomial
+         * Use by ( Source : Wikipedia https://en.wikipedia.org/wiki/Cyclic_redundancy_check#Commonly_used_and_standardized_CRCs ) :
+         * ISO 3309 (HDLC), ANSI X3.66 (ADCCP), FIPS PUB 71, FED-STD-1003, ITU-type_t V.42, ISO/IEC/IEEE 802-3 (Ethernet), SATA, MPEG-2, PKZIP, Gzip, Bzip2, POSIX cksum,PNG, ZMODEM, many others
+         */
+        static constexpr u32 REFLECTED_POLYNOMIAL = 0xedb88320;
+
+        /** LookUpTable generated with polynomial 0xedb88320 */
+        static const u32 CRC32_LOOKUP[8][256];
+
+        /** Table used for combine */
+        static const u32 X2N_TABLE[32];
+
+        /**
          * CRC32 hash based on Intel slice-by-8 algorithm
          * Polynomial used is 0x04C11DB7
          * @param buffer Pointer to the first byte to hash
@@ -15,14 +35,15 @@ namespace hud::hash_algorithm
          * @param seed The seed to use. Default is 0.
          * @return The CRC32 of the buffer
          */
-        [[nodiscard]] static HD_CORE_DLL u32 hash(const u8 *buffer, usize count, const u32 seed = 0) noexcept;
+        [[nodiscard]] static HD_CORE_DLL u32
+        hash(const u8 *buffer, usize count, const u32 seed = 0) noexcept;
 
         /**
-         * Check that the CRC32 lookup table have correct values
-         * Intent to be used only for test validation
+         * Combine 2 crc32 hash
+         * @param crc1 The first crc
+         * @param crc2 The second crc
+         * @param count_crc2 The count of bytes used to compute the crc2
          */
-        [[nodiscard]] static HD_CORE_DLL bool is_lookup_table_values_correct() noexcept;
-
         [[nodiscard]] static HD_CORE_DLL u32 combine(u32 crc1, u32 crc2, usize count_crc2) noexcept;
     };
 
--- test/hash/crc32.cpp
+++ test/hash/crc32.cpp
@@ -1,14 +1,49 @@
 #include <core/hash/crc32.h>
 #include <slice_by_8/crc.h> // Intel Slice-by-8
+// #include <zlib.h>           // zlib
 
 #include <core/cstring.h>
 
 static constexpr const ansichar *txt = "abcdefghijklmnopqrstuvwxyz";
 static constexpr const ansichar *txt_unaligned = " abcdefghijklmnopqrstuvwxyz";
+static constexpr const ansichar *txt_test = "123456789";
 
 GTEST_TEST(hash, crc32_lookup_table_is_correct)
 {
-    hud_assert_true(hud::hash_algorithm::crc32::is_lookup_table_values_correct());
+    // Generate the Crc32 Lookup table with the correct polynomial
+    u32 generated_crc32_lookup[8][256] = {};
+
+    for (unsigned int i = 0; i <= 0xFF; i++)
+    {
+        uint32_t crc = i;
+        for (unsigned int j = 0; j < 8; j++)
+        {
+            crc = (crc >> 1) ^ ((crc & 1) * hud::hash_algorithm::crc32::REFLECTED_POLYNOMIAL);
+        }
+        generated_crc32_lookup[0][i] = crc;
+    }
+
+    for (unsigned int i = 0; i <= 0xFF; i++)
+    {
+        generated_crc32_lookup[1][i] = (generated_crc32_lookup[0][i] >> 8) ^ generated_crc32_lookup[0][generated_crc32_lookup[0][i] & 0xFF];
+        generated_crc32_lookup[2][i] = (generated_crc32_lookup[1][i] >> 8) ^ generated_crc32_lookup[0][generated_crc32_lookup[1][i] & 0xFF];
+        generated_crc32_lookup[3][i] = (generated_crc32_lookup[2][i] >> 8) ^ generated_crc32_lookup[0][generated_crc32_lookup[2][i] & 0xFF];
+        generated_crc32_lookup[4][i] = (generated_crc32_lookup[3][i] >> 8) ^ generated_crc32_lookup[0][generated_crc32_lookup[3][i] & 0xFF];
+        generated_crc32_lookup[5][i] = (generated_crc32_lookup[4][i] >> 8) ^ generated_crc32_lookup[0][generated_crc32_lookup[4][i] & 0xFF];
+        generated_crc32_lookup[6][i] = (generated_crc32_lookup[5][i] >> 8) ^ generated_crc32_lookup[0][generated_crc32_lookup[5][i] & 0xFF];
+        generated_crc32_lookup[7][i] = (generated_crc32_lookup[6][i] >> 8) ^ generated_crc32_lookup[0][generated_crc32_lookup[6][i] & 0xFF];
+    }
+
+    // Assert that all values in the lookup table are corrects
+    for (u32 i = 0; i < 8; i++)
+    {
+        for (u32 j = 0; j < 256; j++)
+        {
+            hud::check(generated_crc32_lookup[i][j] == hud::hash_algorithm::crc32::CRC32_LOOKUP[i][j]);
+        }
+    }
+    // Compare with the crc32 lookup table
+    hud_assert_true(hud::memory::compare_equal(hud::hash_algorithm::crc32::CRC32_LOOKUP, generated_crc32_lookup));
 }
 
 GTEST_TEST(hash, crc32_hash_aligned_buffer)
@@ -33,6 +68,57 @@ GTEST_TEST(hash, crc32_result_same_as_intel_implementation)
 {
     u32 runningcrc = ~0u;
     hud_assert_eq(hud::hash_algorithm::crc32::hash((const u8 *)(txt), hud::cstring::length(txt), 0), ~crc32_sb8_64_bit(&runningcrc, (const u8 *)(txt), static_cast<uint32_t>(hud::cstring::length(txt)), 0, MODE_CONT));
-
     hud_assert_eq(hud::hash_algorithm::crc32::hash((const u8 *)(txt_unaligned + 1), hud::cstring::length(txt_unaligned + 1), 0), ~crc32_sb8_64_bit(&runningcrc, (const u8 *)(txt_unaligned + 1), static_cast<uint32_t>(hud::cstring::length(txt_unaligned + 1)), 0, MODE_CONT));
+}
+
+// From zlib crc32.c
+static u32 multmodp(u32 a, u32 b)
+{
+    u32 m, p;
+
+    m = (u32)1 << 31;
+    p = 0;
+    for (;;)
+    {
+        if (a & m)
+        {
+            p ^= b;
+            if ((a & (m - 1)) == 0)
+                break;
+        }
+        m >>= 1;
+        b = b & 1 ? (b >> 1) ^ hud::hash_algorithm::crc32::REFLECTED_POLYNOMIAL : b >> 1;
+    }
+    return p;
+}
+
+GTEST_TEST(hash, crc32_lookup_x2ntable_is_correct)
+{
+    // Generate the x2n_table
+    u32 p = (u32)1 << 30; /* x^1 */
+
+    u32 x2n_table[32];
+    x2n_table[0] = p;
+    for (u32 n = 1; n < 32; n++)
+    {
+        x2n_table[n] = p = multmodp(p, p);
+    }
+    // Compare with the crc32 x2n_table
+    hud_assert_true(hud::memory::compare_equal(hud::hash_algorithm::crc32::X2N_TABLE, x2n_table));
+}
+
+GTEST_TEST(hash, 123456789)
+{
+    // From https://github.com/Michaelangel007/crc32
+    const u32 result = hud::hash_algorithm::crc32::hash((const u8 *)(txt_test), hud::cstring::length(txt_test), 0);
+    hud_assert_eq(result, 0xCBF43926);
+}
+
+GTEST_TEST(hash, combine)
+{
+    const u32 result_1 = hud::hash_algorithm::crc32::hash((const u8 *)(txt), hud::cstring::length(txt), 0);
+    const u32 result_2 = hud::hash_algorithm::crc32::hash((const u8 *)(txt_test), hud::cstring::length(txt_test), 0);
+    const u32 result = hud::hash_algorithm::crc32::combine(result_1, result_2, hud::cstring::length(txt_test));
+
+    // hud_assert_eq(result, 0x);
 }
