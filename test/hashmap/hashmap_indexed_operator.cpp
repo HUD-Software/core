7a149c99674bce9c202f083b69dbaf89b7312037
Julian LALU <julian.lalu@outlook.fr>
2025-08-07 18:08:31 +0200

    Fix hashmap tests

--- test/hashmap/hashmap_indexed_operator.cpp
+++ test/hashmap/hashmap_indexed_operator.cpp
@@ -4,19 +4,68 @@ GTEST_TEST(hashmap, indexed_operator_trivial_same_type)
 {
     const auto test = []()
     {
-        hud::hashmap<const char *, const char *> map;
-        const char key_0[] = "key";
-        const char value_0[] = "value";
-        const char value_1[] = "value_1";
-        // Should add default value ""
-        map[key_0];
-        const bool ok_default = hud::cstring::equals(map.find(key_0)->value(), "");
-        // Should add "key_0", "value_0"
-        map[key_0] = value_0;
-        const bool ok = map.find(key_0)->value() = value_0;
-        // Should replace value of "key_0" to "value_1"
-        map[key_0] = value_1;
-        const bool ok_replace = map.find(key_0)->value() = value_1;
+        using key_type = u32;
+        using value_type = u64;
+        hud::hashmap<key_type, value_type> map;
+
+        const key_type key = 123;
+        // Should add default value 0
+        map[key];
+        const bool ok_default = map.find(key)->value() == 0;
+        // Should add key, 0
+        map[key] = value_type {1};
+        const bool ok = map.find(key)->value() == 1;
+        // Should replace value of key to 2
+        map[key] = value_type {2};
+        const bool ok_replace = map.find(key)->value() == 2;
+
+        return std::tuple {
+            ok_default,
+            ok,
+            ok_replace
+        };
+    };
+
+    // Non constant
+    {
+        const auto result = test();
+        hud_assert_true(std::get<0>(result));
+        hud_assert_true(std::get<1>(result));
+        hud_assert_true(std::get<2>(result));
+    }
+    // Constant
+    {
+        constexpr auto result = test();
+        hud_assert_true(std::get<0>(result));
+        hud_assert_true(std::get<1>(result));
+        hud_assert_true(std::get<2>(result));
+    }
+}
+
+GTEST_TEST(hashmap, indexed_operator_trivial_different_type)
+{
+    const auto test = []()
+    {
+        using key_type = u32;
+        using value_type = u64;
+
+        using other_key_type = i16;
+        using other_value_type = i8;
+        static_assert(!hud::is_same_v<key_type, other_key_type>);
+        static_assert(!hud::is_same_v<value_type, other_value_type>);
+
+        hud::hashmap<key_type, value_type> map;
+        const other_key_type key = 123;
+
+        // Should add default value 0
+        map[key];
+        const bool ok_default = map.find(key)->value() == 0;
+        // Should add key, 0
+        map[key] = other_value_type {1};
+        const bool ok = map.find(key)->value() == 1;
+        // Should replace value of key to 2
+        map[key] = other_value_type {2};
+        const bool ok_replace = map.find(key)->value() == 2;
 
         return std::tuple {
             ok_default,
