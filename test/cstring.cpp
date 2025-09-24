16375cac056ac9dcbaba3548e94b404390eb4e4e
Julian LALU <julian.lalu@outlook.fr>
2025-09-24 17:36:09 +0200

    Add lipsum tests

--- test/cstring.cpp
+++ test/cstring.cpp
@@ -1,6 +1,14 @@
 #include <core/cstring.h>
 #include "string/lipsum/latin_lipsum.h"
 #include "string/lipsum/russian_lipsum.h"
+#include "string/lipsum/korean_lipsum.h"
+#include "string/lipsum/japanese_lipsum.h"
+#include "string/lipsum/hindi_lipsum.h"
+#include "string/lipsum/hebrew_lipsum.h"
+#include "string/lipsum/emoji_lipsum.h"
+#include "string/lipsum/chinese_lipsum.h"
+#include "string/lipsum/arabic_lipsum.h"
+
 namespace hud_test
 {
     template<typename TCharType>
@@ -72,20 +80,83 @@ GTEST_TEST(cstring, is_vaid_utf8)
         const char8 russian_lipsum[] = RUSSIAN_LIPSUM;
         return hud::cstring::is_valid_utf8(RUSSIAN_LIPSUM, sizeof(russian_lipsum));
     };
+    const auto test_korean = []() {
+        // asm volatile("nop");
+        const char8 korean_lipsum[] = KOREAN_LIPSUM;
+        return hud::cstring::is_valid_utf8(KOREAN_LIPSUM, sizeof(korean_lipsum));
+    };
+    const auto test_japanese = []() {
+        // asm volatile("nop");
+        const char8 japanese_lipsum[] = JAPANESE_LIPSUM;
+        return hud::cstring::is_valid_utf8(JAPANESE_LIPSUM, sizeof(japanese_lipsum));
+    };
+    const auto test_hindi = []() {
+        // asm volatile("nop");
+        const char8 hindi_lipsum[] = HINDI_LIPSUM;
+        return hud::cstring::is_valid_utf8(HINDI_LIPSUM, sizeof(hindi_lipsum));
+    };
+    const auto test_hebrew = []() {
+        // asm volatile("nop");
+        const char8 hebrew_lipsum[] = HEBREW_LIPSUM;
+        return hud::cstring::is_valid_utf8(HEBREW_LIPSUM, sizeof(hebrew_lipsum));
+    };
+    const auto test_emoji = []() {
+        // asm volatile("nop");
+        const char8 emoji_lipsum[] = EMOJI_LIPSUM;
+        return hud::cstring::is_valid_utf8(EMOJI_LIPSUM, sizeof(emoji_lipsum));
+    };
+    const auto test_chinese = []() {
+        // asm volatile("nop");
+        const char8 chinese_lipsum[] = CHINESE_LIPSUM;
+        return hud::cstring::is_valid_utf8(CHINESE_LIPSUM, sizeof(chinese_lipsum));
+    };
+    const auto test_arabic = []() {
+        // asm volatile("nop");
+        const char8 arabic_lipsum[] = ARABIC_LIPSUM;
+        return hud::cstring::is_valid_utf8(ARABIC_LIPSUM, sizeof(arabic_lipsum));
+    };
     // Non constant
     {
         const auto result_latin = test_latin();
         hud_assert_true(result_latin);
         const auto result_russian = test_russian();
         hud_assert_true(result_russian);
+        const auto result_korean = test_korean();
+        hud_assert_true(result_korean);
+        const auto result_japanese = test_japanese();
+        hud_assert_true(result_japanese);
+        const auto result_hindi = test_hindi();
+        hud_assert_true(result_hindi);
+        const auto result_hebrew = test_hebrew();
+        hud_assert_true(result_hebrew);
+        const auto result_emoji = test_emoji();
+        hud_assert_true(result_emoji);
+        const auto result_chinese = test_chinese();
+        hud_assert_true(result_chinese);
+        const auto result_arabic = test_arabic();
+        hud_assert_true(result_arabic);
     }
 
     // Constant
     {
-        constexpr auto result_latin = test_latin();
+        const auto result_latin = test_latin();
         hud_assert_true(result_latin);
-        constexpr auto result_russian = test_russian();
+        const auto result_russian = test_russian();
         hud_assert_true(result_russian);
+        constexpr auto result_korean = test_korean();
+        hud_assert_true(result_korean);
+        constexpr auto result_japanese = test_japanese();
+        hud_assert_true(result_japanese);
+        constexpr auto result_hindi = test_hindi();
+        hud_assert_true(result_hindi);
+        constexpr auto result_hebrew = test_hebrew();
+        hud_assert_true(result_hebrew);
+        constexpr auto result_emoji = test_emoji();
+        hud_assert_true(result_emoji);
+        constexpr auto result_chinese = test_chinese();
+        hud_assert_true(result_chinese);
+        constexpr auto result_arabic = test_arabic();
+        hud_assert_true(result_arabic);
     }
 }
 
