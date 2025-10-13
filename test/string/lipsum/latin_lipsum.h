6173a956e8756eb8dc380acbc9035bd4c600d2ad
Julian LALU <julian.lalu@outlook.fr>
2025-10-13 15:03:15 +0200

    Fix too big string

--- test/string/lipsum/latin_lipsum.h
+++ test/string/lipsum/latin_lipsum.h
@@ -14,18 +14,4 @@ Tritani recteque postulant qui ea, vis semper doctus offendit ea. Zril facilisis
 \
 Nec ut adolescens sententiae, nonumes mediocrem molestiae vix at. Inani explicari ei sea, ferri minimum maiestatis his eu. Odio adipiscing suscipiantur id vix, ea eum fugit commodo erroribus, ex duis labore meliore sed. An pri dico audire deseruisse, atqui offendit lobortis per te. Eum no tation evertitur, cum populo indoctum ex.\
 \
-Sanctus quaerendum ei eum. Sumo deserunt sit ne, id elit intellegam conclusionemque vim. Movet regione sea no, id clita neglegentur vel? Doming electram convenire mei eu, sed integre ocurreret ea. In vel sumo nihil suscipiantur.\
-\
-Duo in eius tempor. Mea quas expetenda no, gubergren aliquando argumentum ne vix, at deleniti luptatum suavitate duo! His at quando accusam percipitur, sit errem perpetua consequat te. Justo malorum menandri duo te.\
-\
-Has ea mundi mollis maiorum, debet imperdiet vim ne. Eu ius amet ignota democritum, euismod ullamcorper ei vel? In nec congue delicata repudiare, semper indoctum ad duo. Id nec iuvaret volumus, habeo regione gubergren id nam, quas solum blandit eam ei. Sea ea causae aperiam suscipit, pri ex illum expetendis scripserit. Qui ad velit errem.\
-\
-In pri dicta aperiam tritani, assentior tincidunt reprehendunt sit ex! Ut sea numquam invidunt! Ea nobis delectus instructior eam, audire oblique expetenda has ut, ne nostro feugiat ullamcorper per! Novum voluptatum ei vix, eam an fugit albucius, ut nonumy omnium voluptatum quo. An vis tibique convenire iudicabit, ex tation fuisset voluptatum his!\
-\
-Delectus fabellas reformidans ea nam, in dolores reprimique pri! Elitr definitiones necessitatibus eum eu. Iusto oporteat quo ea, sed ut cibo menandri appellantur. Eius justo consetetur pri in. Mazim adipiscing necessitatibus has et, mel cibo putant intellegat te!\
-\
-Id his salutandi mediocritatem. Solet ornatus at nam! Ut vide delectus quo! Cu nec deseruisse adversarium, ei vivendum scribentur his, eu alienum consulatu assueverit qui? Nam wisi electram mnesarchum at? Has eirmod tincidunt ne, soleat commodo accusam est ad, sadipscing temporibus sit ei?\
-\
-Vis liber numquam sanctus te, quando putant timeam sea id, ne usu dicat vidisse omnesque. Ea audire iracundia vix, virtute voluptatum at cum, vim at vitae erroribus gloriatur. Mel ex ornatus cotidieque vituperatoribus, per ut quas altera! Cum cu modo referrentur? Pri ad nibh imperdiet consequat, an veritus intellegat sed. Facete prompta nostrud te est!\
-\
-In everti concludaturque nam! Veritus ceteros mediocrem usu ne! Augue placerat no per? Pro justo omnium id? Munere feugait nam ea! Mel ad elitr dolor, ius no erat dicam iracundia, cu sadipscing conclusionemque vix."
+Sanctus quaerendum ei eum. Sumo deserunt sit ne, id elit intellegam conclusionemque vim. Movet regione sea no, id clita neglegentur vel? Doming electram convenire mei eu, sed integre ocurreret ea. In vel sumo nihil suscipiantur."
