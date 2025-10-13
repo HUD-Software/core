69caa9d4f80c8fb66038a31c8e2b7be9f9d16797
Julian LALU <julian.lalu@outlook.fr>
2025-10-13 14:42:01 +0200

    Improve compile time build and fix compilations

--- test/traits/traits_underlying_type.cpp
+++ test/traits/traits_underlying_type.cpp
@@ -22,56 +22,6 @@ namespace hud_test
     enum class e16 : isize;
     enum class e17 : usize;
 
-    enum class e2c : const i8;
-    enum class e3c : const i16;
-    enum class e4c : const i32;
-    enum class e5c : const i64;
-    enum class e6c : const u8;
-    enum class e7c : const u16;
-    enum class e8c : const u32;
-    enum class e9c : const u64;
-    enum class e10c : const char8;
-    enum class e11c : const wchar;
-    enum class e12c : const char16;
-    enum class e13c : const char32;
-    enum class e14c : const iptr;
-    enum class e15c : const uptr;
-    enum class e16c : const isize;
-    enum class e17c : const usize;
-
-    enum class e2v : volatile i8;
-    enum class e3v : volatile i16;
-    enum class e4v : volatile i32;
-    enum class e5v : volatile i64;
-    enum class e6v : volatile u8;
-    enum class e7v : volatile u16;
-    enum class e8v : volatile u32;
-    enum class e9v : volatile u64;
-    enum class e10v : volatile char8;
-    enum class e11v : volatile wchar;
-    enum class e12v : volatile char16;
-    enum class e13v : volatile char32;
-    enum class e14v : volatile iptr;
-    enum class e15v : volatile uptr;
-    enum class e16v : volatile isize;
-    enum class e17v : volatile usize;
-
-    enum class e2cv : const volatile i8;
-    enum class e3cv : const volatile i16;
-    enum class e4cv : const volatile i32;
-    enum class e5cv : const volatile i64;
-    enum class e6cv : const volatile u8;
-    enum class e7cv : const volatile u16;
-    enum class e8cv : const volatile u32;
-    enum class e9cv : const volatile u64;
-    enum class e10cv : const volatile char8;
-    enum class e11cv : const volatile wchar;
-    enum class e12cv : const volatile char16;
-    enum class e13cv : const volatile char32;
-    enum class e14cv : const volatile iptr;
-    enum class e15cv : const volatile uptr;
-    enum class e16cv : const volatile isize;
-    enum class e17cv : const volatile usize;
 } // namespace hud_test
 
 GTEST_TEST(traits, underlying_type)
@@ -98,55 +48,4 @@ GTEST_TEST(traits, underlying_type)
     hud_assert_true((hud::is_same_v<hud::underlying_type_t<hud_test::e15>, uptr>));
     hud_assert_true((hud::is_same_v<hud::underlying_type_t<hud_test::e16>, isize>));
     hud_assert_true((hud::is_same_v<hud::underlying_type_t<hud_test::e17>, usize>));
-
-    hud_assert_true((hud::is_same_v<hud::underlying_type_t<hud_test::e2c>, i8>));
-    hud_assert_true((hud::is_same_v<hud::underlying_type_t<hud_test::e3c>, i16>));
-    hud_assert_true((hud::is_same_v<hud::underlying_type_t<hud_test::e4c>, i32>));
-    hud_assert_true((hud::is_same_v<hud::underlying_type_t<hud_test::e5c>, i64>));
-    hud_assert_true((hud::is_same_v<hud::underlying_type_t<hud_test::e6c>, u8>));
-    hud_assert_true((hud::is_same_v<hud::underlying_type_t<hud_test::e7c>, u16>));
-    hud_assert_true((hud::is_same_v<hud::underlying_type_t<hud_test::e8c>, u32>));
-    hud_assert_true((hud::is_same_v<hud::underlying_type_t<hud_test::e9c>, u64>));
-    hud_assert_true((hud::is_same_v<hud::underlying_type_t<hud_test::e10c>, char8>));
-    hud_assert_true((hud::is_same_v<hud::underlying_type_t<hud_test::e11c>, wchar>));
-    hud_assert_true((hud::is_same_v<hud::underlying_type_t<hud_test::e12c>, char16>));
-    hud_assert_true((hud::is_same_v<hud::underlying_type_t<hud_test::e13c>, char32>));
-    hud_assert_true((hud::is_same_v<hud::underlying_type_t<hud_test::e14c>, iptr>));
-    hud_assert_true((hud::is_same_v<hud::underlying_type_t<hud_test::e15c>, uptr>));
-    hud_assert_true((hud::is_same_v<hud::underlying_type_t<hud_test::e16c>, isize>));
-    hud_assert_true((hud::is_same_v<hud::underlying_type_t<hud_test::e17c>, usize>));
-
-    hud_assert_true((hud::is_same_v<hud::underlying_type_t<hud_test::e2v>, i8>));
-    hud_assert_true((hud::is_same_v<hud::underlying_type_t<hud_test::e3v>, i16>));
-    hud_assert_true((hud::is_same_v<hud::underlying_type_t<hud_test::e4v>, i32>));
-    hud_assert_true((hud::is_same_v<hud::underlying_type_t<hud_test::e5v>, i64>));
-    hud_assert_true((hud::is_same_v<hud::underlying_type_t<hud_test::e6v>, u8>));
-    hud_assert_true((hud::is_same_v<hud::underlying_type_t<hud_test::e7v>, u16>));
-    hud_assert_true((hud::is_same_v<hud::underlying_type_t<hud_test::e8v>, u32>));
-    hud_assert_true((hud::is_same_v<hud::underlying_type_t<hud_test::e9v>, u64>));
-    hud_assert_true((hud::is_same_v<hud::underlying_type_t<hud_test::e10v>, char8>));
-    hud_assert_true((hud::is_same_v<hud::underlying_type_t<hud_test::e11v>, wchar>));
-    hud_assert_true((hud::is_same_v<hud::underlying_type_t<hud_test::e12v>, char16>));
-    hud_assert_true((hud::is_same_v<hud::underlying_type_t<hud_test::e13v>, char32>));
-    hud_assert_true((hud::is_same_v<hud::underlying_type_t<hud_test::e14v>, iptr>));
-    hud_assert_true((hud::is_same_v<hud::underlying_type_t<hud_test::e15v>, uptr>));
-    hud_assert_true((hud::is_same_v<hud::underlying_type_t<hud_test::e16v>, isize>));
-    hud_assert_true((hud::is_same_v<hud::underlying_type_t<hud_test::e17v>, usize>));
-
-    hud_assert_true((hud::is_same_v<hud::underlying_type_t<hud_test::e2cv>, i8>));
-    hud_assert_true((hud::is_same_v<hud::underlying_type_t<hud_test::e3cv>, i16>));
-    hud_assert_true((hud::is_same_v<hud::underlying_type_t<hud_test::e4cv>, i32>));
-    hud_assert_true((hud::is_same_v<hud::underlying_type_t<hud_test::e5cv>, i64>));
-    hud_assert_true((hud::is_same_v<hud::underlying_type_t<hud_test::e6cv>, u8>));
-    hud_assert_true((hud::is_same_v<hud::underlying_type_t<hud_test::e7cv>, u16>));
-    hud_assert_true((hud::is_same_v<hud::underlying_type_t<hud_test::e8cv>, u32>));
-    hud_assert_true((hud::is_same_v<hud::underlying_type_t<hud_test::e9cv>, u64>));
-    hud_assert_true((hud::is_same_v<hud::underlying_type_t<hud_test::e10cv>, char8>));
-    hud_assert_true((hud::is_same_v<hud::underlying_type_t<hud_test::e11cv>, wchar>));
-    hud_assert_true((hud::is_same_v<hud::underlying_type_t<hud_test::e12cv>, char16>));
-    hud_assert_true((hud::is_same_v<hud::underlying_type_t<hud_test::e13cv>, char32>));
-    hud_assert_true((hud::is_same_v<hud::underlying_type_t<hud_test::e14cv>, iptr>));
-    hud_assert_true((hud::is_same_v<hud::underlying_type_t<hud_test::e15cv>, uptr>));
-    hud_assert_true((hud::is_same_v<hud::underlying_type_t<hud_test::e16cv>, isize>));
-    hud_assert_true((hud::is_same_v<hud::underlying_type_t<hud_test::e17cv>, usize>));
 }
